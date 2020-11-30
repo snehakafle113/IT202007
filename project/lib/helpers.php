06cf8b14019b843e5397f2bcb7b2b420e4cf04c3
Sneha Kafle <sk2755@ip-172-31-94-251.ec2.internal>
2020-11-30 00:28:13 +0000

    put do_transaction function in helpers for easier access

--- project/lib/helpers.php
+++ project/lib/helpers.php
@@ -67,5 +67,59 @@ function getMessages() {
     return array();
 }
 
+function do_transaction($acc1, $acc2, $amount, $memo, $type){
+    $db = getDB();
+    $query = null;
+    $stmt2 = $db->prepare("SELECT SUM(amount) as balance FROM Transactions WHERE Transactions.act_src_id = :id");
+    $r2 = $stmt2->execute([":id"=>$acc1]);
+    $result = $stmt2->fetch(PDO::FETCH_ASSOC);
+    $a1tot = (int)$result["balance"];
+    $r2 = $stmt2->execute([
+        ":id"=>$acc2
+    ]);
+    $result = $stmt2->fetch(PDO::FETCH_ASSOC);
+    $a2tot = (int)$result["balance"];
+
+
+//  if($a1tot+$amountChange >= 0)
+    // {
+    $query = "INSERT INTO `Transactions` (`act_src_id`, `act_dest_id`, `amount`, `action_type`, `expected_total`, `memo`) 
+  	VALUES(:p1a1, :p1a2, :p1change, :type, :a1tot, :memo), 
+  			(:p2a1, :p2a2, :p2change, :type, :a2tot, :memo)";
+
+    $stmt = $db->prepare($query);
+    $stmt->bindValue(":p1a1", $acc1);
+    $stmt->bindValue(":p1a2", $acc2);
+    $stmt->bindValue(":p1change", $amount);
+    $stmt->bindValue(":type", $type);
+    $stmt->bindValue(":a1tot", $a1tot+$amount);
+    $stmt->bindValue(":memo", $memo);
+    //flip data for other half of transaction
+    $stmt->bindValue(":p2a1", $acc2);
+    $stmt->bindValue(":p2a2", $acc1);
+    $stmt->bindValue(":p2change", ($amount*-1));
+    $stmt->bindValue(":type", $type);
+    $stmt->bindValue(":a2tot", $a2tot-$amount);
+    $stmt->bindValue(":memo", $memo);
+    $result = $stmt->execute();
+    if ($result) {
+        flash("Created successfully with id: " . $db->lastInsertId());
+    }
+    else {
+        $e = $stmt->errorInfo();
+        flash("Error creating: " . var_export($e, true));
+    }
+
+    $stmt = $db->prepare("UPDATE Accounts SET balance = (SELECT SUM(amount) FROM Transactions WHERE Transactions.act_src_id = :id) where id = :id");
+    $r = $stmt->execute([
+        ":id"=>$acc1
+    ]);
+    $r = $stmt->execute([
+        ":id"=>$acc2
+    ]);
+
+    return $result;
+}
+
 //end flash
 ?>
