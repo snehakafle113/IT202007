eac31b3ed2bf39d1763fdd4fc70e7a8dc42dea7a
Sneha Kafle <sk2755@ip-172-31-94-251.ec2.internal>
2020-12-17 05:54:18 +0000

    updated admin role for lookup

--- project/admin_lookup.php
+++ project/admin_lookup.php
@@ -7,34 +7,33 @@ if (!has_role("Admin")) {
 ?>
 
 <?php
-    $db = getDB();
-    $stmt = $db->prepare("SELECT * from Users");
-    $r = $stmt->execute();
-    $results=[];
-    if($r)
-        $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
-        $F_L_name = null;
+$db = getDB();
+$stmt = $db->prepare("SELECT * from Users");
+$r = $stmt->execute();
+$results=[];
+$F_L_name = null;
+if($r) {
+    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+}
 
-        if(isset($_POST["lookup"])){
-            $F_L_name = $_POST['name'];
+if(isset($_POST["searchUser"])){
+    $F_L_name = $_POST['name'];
+}
 
-        }
+$accountNum = null;
+if(isset($_POST["searchAcc"])){
+    $accountNum = $_POST['accountNum'];
+}
 
-        $accountNum = null;
+$stmt2 = $db->prepare("SELECT * from Accounts");
+$r2 = $stmt2->execute();
+$results2=[];
+if($r2) {
+    $results2 = $stmt2->fetchAll(PDO::FETCH_ASSOC);
+}
 
-        if(isset($_POST["lookupUser"])){
-            $accountNum = $_POST['accountNum'];
-        }
-
-    $stmt2 = $db->prepare("SELECT * from Accounts where account_number = :q");
-    $r2 = $stmt2->execute([":q" => $accountNum]);
-    $results2=[];
-    if($r2) {
-        $results2 = $stmt2->fetchAll(PDO::FETCH_ASSOC);
-    }
-
-    ?>
-<div style="background: #7f94b2; font-size: 20px; padding: 10px; border: 1px solid lightgray; margin: 10px;">
+?>
+    <div style="background: #7f94b2; font-size: 20px; padding: 10px; border: 1px solid lightgray; margin: 10px;">
         <label>Search for Users</label>
         <div class="results">
             <?php if (count($results) > 0): ?>
@@ -64,42 +63,44 @@ if (!has_role("Admin")) {
             <?php endif; ?>
         </div>
         <br>
-	<div style="background: #7f94b2; font-size: 20px; padding: 10px; border: 1px solid lightgray; margin: 10px;">
-        <label>Search for Account</label>
-        <div class="results">
-            <?php if (count($results2) > 0): ?>
-                <div class="list-group">
-                    <?php foreach ($results2 as $res2): ?>
-                        <div class="list-group-item">
-                            <div>
-                                <div>Account Number: <?php safer_echo($res2['accountNum']); ?> </div>
-                                <a type="button" href="profile.php?id=<?php safer_echo($r['id']); ?>"><?php safer_echo($res2['id']); ?>">Transaction History</a>
+        <div style="background: #7f94b2; font-size: 20px; padding: 10px; border: 1px solid lightgray; margin: 10px;">
+            <label>Search for Account</label>
+            <div class="results">
+                <?php if (count($results2) > 0): ?>
+                    <div class="list-group">
+                        <?php foreach ($results2 as $res2): ?>
+                            <div class="list-group-item">
+                                <div>
+                                    <div>Account Number:</div>
+				    <?php safer_echo($res2['account_number'])?>
+                                    <br>
+				    <a type="button" href="transaction_history.php?id=<?php safer_echo($r['id']); ?>">Transaction History</a>
+                                </div>
+                                <br>
                             </div>
-                            <br>
-                        </div>
-                    <?php endforeach; ?>
-                </div>
-            <?php else: ?>
-                <p>No results</p>
-            <?php endif; ?>
+                        <?php endforeach; ?>
+                    </div>
+                <?php else: ?>
+                    <p>No results</p>
+                <?php endif; ?>
+            </div>
         </div>
-    </div>
 
         <form method="POST">
-	    <div class = "form-group">
-            <label>Search Users</label>
-            <br>
-            <input class = "form-control" type="text" placeholder="Enter First or Last Name" name="name"/>
+            <div class = "form-group">
+                <label>Search Users</label>
+                <br>
+                <input class = "form-control" type="text" placeholder="Enter First or Last Name" name="name"/>
             </div>
-	    <input type="submit" name="lookup" value="Lookup"/>
+            <input type="submit" name="searchUser" value="Lookup"/>
             <br>
-	    <div class = "form-group">
-            <label>Search Accounts</label>
-            <input class = "form-control" type="int" maxlength="12" placeholder="Account Number" name="accountNum"/>
-            <input type="submit" name="lookupUser" value="Lookup"/>
+            <div class = "form-group">
+                <label>Search Accounts</label>
+                <input class = "form-control" type="int" maxlength="12" placeholder="Account Number" name="accountNum"/>
+                <input type="submit" name="searchAcc" value="Lookup"/>
             </div>
-	</form>
+        </form>
 
-</div>
+    </div>
 
 <?php require(__DIR__ . "/partials/flash.php");
